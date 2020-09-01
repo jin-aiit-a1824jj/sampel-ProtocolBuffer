53bccfcbdbb37a4d5b3daf076dcdce61f8b8f4a3
a1824jj <a1824jj@aiit.ac.jp>
2020-09-01 09:15:54 +0000

    JAVA options

---
+++ session6-java/src/main/java/simple/proto/OptionMain.java
@@ -0,0 +1,9 @@
+package simple.proto;
+
+import com.example.options.OptionMessageOther;
+
+public class OptionMain {
+    public static void main(String[] args) {
+        OptionMessageOther other = OptionMessageOther.newBuilder().build();
+    }
+}
